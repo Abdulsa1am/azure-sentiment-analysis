76774e5e9597b5b21c958d996f4bd2eb29612fca
Abdulsalam <2230003739@iau.edu.sa>
2026-02-08 17:41:21 +0300

    .ignore and test.py

---
+++ test.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+
+load_dotenv()
+key = os.getenv("my_key")
+my_endpoint = os.getenv("my_endpoint")
+
+def authenticate_client():
+    keyObject = AzureKeyCredential(key)
+    clint = TextAnalyticsClient(
+            endpoint=my_endpoint, 
+            credential=keyObject)
+    return clint
+
+def sentiment_analysis_example(client):
+    try:
+        text = ["I am so happy"]
+        response = client.analyze_sentiment(documents=text)[0] #0 for only one text input (it will return only 1 output)
+        print(f"Document Sentiment: {response.sentiment}") #final resault 
+        
+        print(f"Overall scores: positive={response.confidence_scores.positive}, "
+              f"neutral={response.confidence_scores.neutral}, "
+              f"negative={response.confidence_scores.negative}")
+        print("Job done")
+    except Exception as err:
+        print(f"Connection Failed: {err}")
+
+if __name__ == "__main__":
+    testclient = authenticate_client()
+    sentiment_analysis_example(testclient)
